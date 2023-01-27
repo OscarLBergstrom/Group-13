61bca352490a38648e4dda45c268122b8400cb9a
jacobtr <jacobtr@kth.se>
2023-01-27 13:38:02 +0100

    feat: Implements lic5 along with tests.
    This commit solves issue #5.

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,7 +1,30 @@
 import pytest
+import numpy as np
+from decide import *
 
 def func(x):
     return x + 1
 
 def test_answer():
     assert func(3) == 4
+
+def test_lic5():
+    nrpoints=2
+    points = np.array([[1,0],[0,2]])
+    assert lic5(nrpoints, points)
+
+    nrpoints=2
+    points = np.array([[0,0],[0,2]])
+    assert not lic5(nrpoints, points)
+
+    nrpoints=2
+    points = np.array([[0,0],[1,2]])
+    assert not lic5(nrpoints, points)
+
+    nrpoints=5
+    points = np.array([[0, 0], [1, 2],[2,3],[3,3],[3,4]])
+    assert not lic5(nrpoints, points)
+
+    nrpoints = 5
+    points = np.array([[0, 0], [1, 2], [2, 3], [4, 3], [3, 4]])
+    assert lic5(nrpoints, points)
