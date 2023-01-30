0c1f16f2a569e149eddb3184ae14b7260b9b1485
Oscar Bergstrom <osbergs@kth.se>
2023-01-30 11:44:14 +0100

    feat #6

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -26,12 +26,19 @@ def test_lic0_false():
 # LIC3
 #########
 
-def test_lic3_unit():
+def test_lic3_false():
     parameters["AREA1"] = 100
     points = [[1,1], [0,0], [0,10],[20,0],[1,0],[2,3]] # Should result in a triangle equal but not greater to 100
     value = lic3(points,len(points))
 
     assert value == False
+
+def test_lic3_true():
+    parameters["AREA1"] = 90
+    points = [[1,1], [0,0], [0,10],[20,0],[1,0],[2,3]] # Should result in a triangle equal but not greater to 100
+    value = lic3(points,len(points))
+
+    assert value == True
 
 def test_lic3_line():
     parameters["AREA1"] = 1
